9726aae82551e4670d40848d5d9edd8e10d30174
krisztian.szekeres <krisztian.szekeres@oxit.hu>
2024-06-11 13:10:03 +0200

    connecting mmpose to the pipeline

--- test_hrnet_wp.py
+++ test_hrnet_wp.py
@@ -4,48 +4,50 @@ import cv2
 from mmcv.image import imread
 from mmpose.registry import VISUALIZERS
 
-radius = 3
-kpt_thr = 0.3
-draw_heatmap = True
-thickness = 1
-alpha = 0.8
-skeleton_style = 'mmpose'
-cfg_options = dict(model=dict(test_cfg=dict(output_heatmaps=True)))
+def main(cfg):
 
-register_all_modules()
+    radius = cfg.radius
+    kpt_thr = cfg.kpt_thr
+    draw_heatmap = cfg.draw_heatmap
+    thickness = cfg.thickness
+    alpha = cfg.alpha
+    skeleton_style = cfg.skeleton_style
+    cfg_options = dict(model=dict(test_cfg=dict(output_heatmaps=True)))
 
-config_file = 'td-hm_hrnet-w48_8xb32-210e_coco-256x192.py'
-checkpoint_file = 'td-hm_hrnet-w48_8xb32-210e_coco-256x192-0e67c616_20220913.pth'
+    register_all_modules()
 
-for i in range(0, 403):
-    img_path = "tests/crop_results_small/" + str(i) +".png"
+    config_file = cfg.config_file
+    checkpoint_file = cfg.checkpoint_file
 
-    model = init_model(config_file, checkpoint_file, device='cuda:0', cfg_options=cfg_options)  # or device='cuda:0'
+    for i in range(0, 403):
+        img_path = cfg.img_path + str(i) + ".png"
 
-    # please prepare an image with person
-    results = inference_topdown(model, img_path)
-    print(type(results[0]))
-    print(len(results))
-    #cv2.imshow("test", results)
+        model = init_model(config_file, checkpoint_file, device='cuda:0', cfg_options=cfg_options)  # or device='cuda:0'
 
-    # init visualizer
-    model.cfg.visualizer.radius = radius
-    model.cfg.visualizer.alpha = alpha
-    model.cfg.visualizer.line_width = thickness
+        # please prepare an image with person
+        results = inference_topdown(model, img_path)
+        print(type(results[0]))
+        print(len(results))
+        #cv2.imshow("test", results)
 
-    visualizer = VISUALIZERS.build(model.cfg.visualizer)
-    visualizer.set_dataset_meta(
-        model.dataset_meta, skeleton_style=skeleton_style)
+        # init visualizer
+        model.cfg.visualizer.radius = radius
+        model.cfg.visualizer.alpha = alpha
+        model.cfg.visualizer.line_width = thickness
+
+        visualizer = VISUALIZERS.build(model.cfg.visualizer)
+        visualizer.set_dataset_meta(
+            model.dataset_meta, skeleton_style=skeleton_style)
 
 
-    img = imread(img_path, channel_order='rgb')
-    visualizer.add_datasample(
-        'result',
-        img,
-        data_sample=results[0],
-        draw_gt=False,
-        draw_bbox=True,
-        kpt_thr=kpt_thr,
-        draw_heatmap=draw_heatmap,
-        skeleton_style=skeleton_style,
-        out_file="output/"+ str(i) + " .png")
+        img = imread(img_path, channel_order='rgb')
+        visualizer.add_datasample(
+            'result',
+            img,
+            data_sample=results[0],
+            draw_gt=False,
+            draw_bbox=True,
+            kpt_thr=kpt_thr,
+            draw_heatmap=draw_heatmap,
+            skeleton_style=skeleton_style,
+            out_file="output/"+ str(i) + " .png")
